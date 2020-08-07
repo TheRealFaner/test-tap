49770db2c41fc784d76f7dfcbe62a5b2e977335a
Vladislav Alexeev <faner.tuz@gmail.com>
2020-08-07 14:47:39 +0700

    Add person popup and update all CSS files

--- test-tap/src/App.js
+++ test-tap/src/App.js
@@ -2,34 +2,43 @@ import React, { useState, useEffect } from 'react';
 import * as DB from './db/db.js';
 import './App.css';
 import { Persons } from './components/persons/persons.js';
-
+import { Popup } from './components/popup/popup.js';
 function App() {
 
   const [isLoaded, setIsLoaded] = useState(false);
   const [persons, setPersons] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
 
+  function togglePopup() {
+    setShowPopup(!showPopup);
+  }
   // Аналогично componentDidMount и componentDidUpdate:
   useEffect(() => {
     async function fetchData() {
       setPersons(await DB.GetAllPersons());
     };
-    // Обновляем заголовок документа с помощью API браузера
     fetchData();
-    console.log(persons);
     setIsLoaded(true);
   }, []);
 
   return (
     <>
     <header>
-      <div>Список сотрудников</div>
+      <span>Список сотрудников</span>
     </header>
     <main className="personsList">
       {isLoaded ? <Persons data={persons}/> : <div></div>}
     </main>
     <footer>
-      <button type="button" className="btn btn-person">Добавить</button>
+      <button type="button" className="btn btn-sucess" onClick={togglePopup}>Добавить</button>
     </footer>
+    {showPopup ? 
+        <Popup
+          header="Добавить сотрудника"
+          togglePopup={togglePopup}
+        />
+        : null
+      }
     </>
   );
 }
--- test-tap/src/components/persons/persons.js
+++ test-tap/src/components/persons/persons.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import './index.css';
 import PersonIcon from '../../icons/persons/person.svg';
+import PersonsIcon from '../../icons/persons/persons.svg';
 import EditIcon from '../../icons/actions/edit.svg';
 import DeleteIcon from '../../icons/actions/delete.svg';
 
 function Person({data}) {
     return  <div className="person">
-                <img src={PersonIcon} className="personIcon"/>
+                <img src={PersonIcon} alt="" className="personIcon"/>
                 <div className="firstName">{data.firstName}</div>
                 <div className="lastName">{data.lastName}</div>
                 <div className="commands">
-                    <div>
-                        <button type="button" className="btn btn-action-edit">
-                            <img src={EditIcon} className="actionImg"/>
-                        </button>
-                    </div>
-                    <div>
-                        <button type="button" className="btn btn-action-delete">
-                            <img src={DeleteIcon} className="actionImg"/>
-                        </button>
-                    </div>
+                <button type="button" className="btn btn-alert btn-action">
+                    <img src={EditIcon} alt="" className="actionImg"/>
+                </button>
+
+                <button type="button" className="btn btn-warning btn-action">
+                    <img src={DeleteIcon} alt="" className="actionImg"/>
+                </button>
                 </div>
             </div>
 }
@@ -31,7 +29,7 @@ export function Persons ({data}) {
 
     return  <>
                 <div className="person">
-                    <img src={PersonIcon} className="personIcon"/>
+                    <img src={PersonsIcon} alt="" className="personIcon"/>
                     <div className="firstName">Имя</div>
                     <div className="lastName">Фамилия</div>
                     <div className="commands">Действия</div>
